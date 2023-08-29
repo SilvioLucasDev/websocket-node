8f9da71bd358cb393718cc72960e45ac5e97d692
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-29 18:30:56 -0300

    feat: add PgGradeRepository

---
+++ src/infra/repositories/postgres/grade.ts
@@ -0,0 +1,25 @@
+import { type GetPointsGrade, type SaveGrade } from '@/application/contracts/repositories'
+import prisma from '@/infra/repositories/postgres/helpers/connection'
+
+export class PgGradeRepository implements SaveGrade, GetPointsGrade {
+  async save ({ id, idStudent, note, points }: SaveGrade.Input): Promise<void> {
+    await prisma.grade.create({
+      data: { id, id_student: idStudent, note, points }
+    })
+  }
+
+  async getPoints ({ id }: GetPointsGrade.Input): Promise<GetPointsGrade.Output> {
+    const points = await prisma.grade.aggregate({
+      _sum: {
+        points: true
+      },
+      where: {
+        id
+      }
+    })
+    if (points._sum.points !== null && points._sum.points !== undefined) {
+      return { points: points._sum.points }
+    }
+    return { points: 0 }
+  }
+}
