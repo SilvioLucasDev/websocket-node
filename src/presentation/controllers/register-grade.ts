62d3ab66cd6a3efab67abb1cf0315a7c9158c322
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-30 13:46:51 -0300

    feat: add call GetRankingUseCase in RegisterGradeController

--- src/presentation/controllers/register-grade.ts
+++ src/presentation/controllers/register-grade.ts
@@ -1,17 +1,21 @@
 import { badRequest, serverError, type HttpResponse, noContent } from '@/presentation/helpers'
-import { type RegisterGradeUseCase } from '@/application/use-cases'
+import { type GetRankingUseCase, type RegisterGradeUseCase } from '@/application/use-cases'
 import { StudentNotFoundError } from '@/application/errors'
 import { RequiredString, RequiredNumber, ValidationComposite } from '@/presentation/validation'
 import { type Controller } from '@/presentation/controllers'
 
 export class RegisterGradeController implements Controller {
-  constructor (private readonly registerGradeUseCase: RegisterGradeUseCase) {}
+  constructor (
+    private readonly registerGradeUseCase: RegisterGradeUseCase,
+    private readonly getRankingUseCase: GetRankingUseCase
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       const error = this.validate(httpRequest)
       if (error !== undefined) return badRequest(error)
       await this.registerGradeUseCase.execute({ note: httpRequest.note, idStudent: httpRequest.idStudent })
+      await this.getRankingUseCase.execute()
       return noContent()
     } catch (error) {
       if (error instanceof StudentNotFoundError) return badRequest(error)
