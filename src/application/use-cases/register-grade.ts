5d811be2023c7e2c1d023162ebb63d946f7ec89c
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-30 13:36:56 -0300

    refactor: remove return of process RegisterGrade

--- src/application/use-cases/register-grade.ts
+++ src/application/use-cases/register-grade.ts
@@ -1,30 +1,24 @@
 import { type UUIDGenerator } from '@/application/contracts/adapters'
-import { type GetPointsGrade, type GetStudent, type SaveGrade } from '@/application/contracts/repositories'
+import { type GetStudent, type SaveGrade } from '@/application/contracts/repositories'
 import { Grade } from '@/domain/entities'
 import { StudentNotFoundError } from '@/application/errors'
 
 export class RegisterGradeUseCase {
   constructor (
     private readonly studentRepository: GetStudent,
-    private readonly gradeRepository: SaveGrade & GetPointsGrade,
+    private readonly gradeRepository: SaveGrade,
     private readonly crypto: UUIDGenerator
   ) {}
 
-  async execute ({ note, idStudent }: Input): Promise<Output> {
+  async execute ({ note, idStudent }: Input): Promise<void> {
     const student = await this.studentRepository.get({ id: idStudent })
     if (student === undefined) throw new StudentNotFoundError()
     const newGrade = Grade.create({ idStudent, note }, this.crypto)
     await this.gradeRepository.save(newGrade)
-    const { points } = await this.gradeRepository.getPoints({ id: idStudent })
-    return { points } // REMOVER ESSE RETORNO E DEIXAR VOID
   }
 }
 
 type Input = {
   note: number
   idStudent: string
-}
-
-type Output = {
-  points: number
 }
--- src/presentation/controllers/register-grade.ts
+++ src/presentation/controllers/register-grade.ts
@@ -1,4 +1,4 @@
-import { badRequest, serverError, type HttpResponse, ok } from '@/presentation/helpers'
+import { badRequest, serverError, type HttpResponse, noContent } from '@/presentation/helpers'
 import { type RegisterGradeUseCase } from '@/application/use-cases'
 import { StudentNotFoundError } from '@/application/errors'
 import { RequiredString, RequiredNumber, ValidationComposite } from '@/presentation/validation'
@@ -11,8 +11,8 @@ export class RegisterGradeController implements Controller {
     try {
       const error = this.validate(httpRequest)
       if (error !== undefined) return badRequest(error)
-      const result = await this.registerGradeUseCase.execute({ note: httpRequest.note, idStudent: httpRequest.idStudent })
-      return ok({ points: result.points })
+      await this.registerGradeUseCase.execute({ note: httpRequest.note, idStudent: httpRequest.idStudent })
+      return noContent()
     } catch (error) {
       if (error instanceof StudentNotFoundError) return badRequest(error)
       return serverError(error as Error)
@@ -31,6 +31,4 @@ type HttpRequest = {
   idStudent: string
 }
 
-type Model = Error | {
-  points: number
-}
+type Model = Error
